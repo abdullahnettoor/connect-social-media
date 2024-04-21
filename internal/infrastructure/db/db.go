a35f38b58c6c9b05dc5c352b4f714bed56dd9588
abdullahnettoor <abdullahnettoor@gmail.com>
2024-04-21 17:13:31 +0530

    💽 Fixed Retry for Db Connection with containerized db uri

--- internal/infrastructure/db/db.go
+++ internal/infrastructure/db/db.go
@@ -9,33 +9,42 @@ import (
 )
 
 func ConnectDb(cfg *config.Config) (neo4j.DriverWithContext, error) {
+	log.Println("NEO4J: Trying to connect with LOCALHOST")
 	driver, err := neo4j.NewDriverWithContext(
 		// Try connecting with localhost
 		cfg.DbUri,
 		neo4j.BasicAuth(cfg.DbUsername, cfg.DbPassword, ""),
 	)
-
-	if neo4j.IsConnectivityError(err) {
-		// Try connecting to containerized db
-		driver, err = neo4j.NewDriverWithContext(
-			cfg.DbContainerUri,
-			neo4j.BasicAuth(cfg.DbUsername, cfg.DbPassword, ""),
-		)
-	}
-
 	if err != nil {
-		log.Println(err)
+		log.Println("NEO4J:", err.Error())
 		return nil, err
 	}
 
-	// defer driver.Close(context.Background())
+	log.Println("NEO4J: Verifying Connectivity")
+	err = driver.VerifyConnectivity(context.Background())
+	if !neo4j.IsConnectivityError(err) && err != nil {
+		log.Println("NEO4J:", err.Error())
+		return nil, err
+	}
 
+	// Try connecting to containerized db
+	log.Println("NEO4J: Trying to connect with CONTAINER")
+	driver, err = neo4j.NewDriverWithContext(
+		cfg.DbContainerUri,
+		neo4j.BasicAuth(cfg.DbUsername, cfg.DbPassword, ""),
+	)
+	if err != nil {
+		log.Println("NEO4J:", err.Error())
+		return nil, err
+	}
+
+	log.Println("NEO4J: Verifying Connectivity")
 	err = driver.VerifyConnectivity(context.Background())
 	if err != nil {
-		log.Println(err)
+		log.Println("NEO4J:", err.Error())
 		return nil, err
 	}
 
-	log.Println("Neo4j Connection Established")
+	log.Println("NEO4J: Connection Established")
 	return driver, nil
 }
